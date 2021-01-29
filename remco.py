a23375f8029d3fa9fcda4dc3a6d0f3f4a4f3e74c
Dmitrij53 <71382095+Dmitrij53@users.noreply.github.com>
2021-01-29 22:00:50 +0200

    Update remco.py

--- remco.py
+++ remco.py
@@ -1,26 +1,26 @@
-import time
-from datetime import datetime as dt
-host_path = "/etc/hosts"
-redirect = "127.0.0.1"
-website_list = ["www.youtube.com", "youtube.com", "www.facebook.com", "facebook.com"]
+import time #bibliotēka importēšana
+from datetime import datetime as dt #bibliotēkas specifikācija
+host_path = "/etc/hosts" #faiļa atrašanas vietas norādīšana
+redirect = "127.0.0.1" #norādam uz kurieniem novirzam klientu
+website_list = ["www.youtube.com", "youtube.com", "www.facebook.com", "facebook.com"] #lietotņu sarakstu definēšana
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,12):
-        print("Vel vajag pastrādāt")
-        file = open(host_path,"r+")
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,12): # if funkcija salīdzina ar lokāla datora laiku
+        print("Vel vajag pastrādāt") #rezultāta izvade
+        file = open(host_path,"r+") #definējam mapi ar read
         content = file.read()
-        for website in website_list:
-            if website in content:
-                pass
+        for website in website_list: #funkcija ierakstu meklēšanai
+            if website in content: 
+                pass #ja ir ignorējam
             else:
-                file.write(redirect + " " + website + "\n")
+                file.write(redirect + " " + website + "\n") #ja iestājās pretējais kritērijs, ierakstam vērtibas
     else:
-        print("Atpūšamies")
-        file = open(host_path,'r+')
-        content = file.readlines()
-        file.seek(0)
-        for line in content:
-            if not any(website in line for website in website_list):
-                file.write(line)
-            file.truncate()
-    time.sleep(5)
+        print("Atpūšamies") #ja laiks nesakrīt izvada šo
+        file = open(host_path,'r+')  #definējam mapi ar read
+        content = file.readlines() 
+        file.seek(0) #funkcija pārbauda vai ir kaut kādi ieraksti no weblist
+        for line in content: #norāda ko jādara ja ir
+            if not any(website in line for website in website_list): #nosakam kritēriju ko meklēt
+                file.write(line) #iedodam vajadzīgas tiesības
+            file.truncate() #izdzēšam
+    time.sleep(5) #snoozy funkciaj
